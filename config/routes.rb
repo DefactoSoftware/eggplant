bb2cc0f7c79b7668192f56eefba6fceb6f4abb3d
Marcel Horlings <marcel.horlings@gmail.com>
2015-11-14 17:56:41 +0100

    test creation of a team

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: "home#index"
+  root to: "static_pages#home"
   get "/dashboard", to: "dashboard#index"
 
   get "/auth/:provider/callback", to: "sessions#create"
